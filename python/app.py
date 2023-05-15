ad8a7b53ea723a521276733d417e9d6301357750
Sydney Melendres <87118077+sydney-melendres@users.noreply.github.com>
2023-05-15 14:00:19 +1000

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello AU Student Again!"
+    return "Hello, it's me."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
